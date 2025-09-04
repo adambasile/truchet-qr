1dc94178b51b4a2ac3192f7c0cb2c8d7d1a4b67f
Adam Basile <basile.adam@gmail.com>
2025-09-04 21:50:40 +1000

    Refining eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,8 +1,9 @@
 import { FlatCompat } from "@eslint/eslintrc";
 import js from "@eslint/js";
-import { defineConfig } from "eslint/config";
+import { defineConfig, globalIgnores } from "eslint/config";
 import path from "node:path";
 import { fileURLToPath } from "node:url";
+import { includeIgnoreFile } from "@eslint/compat";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -12,7 +13,11 @@ const compat = new FlatCompat({
   allConfig: js.configs.all,
 });
 
+const gitignorePath = fileURLToPath(new URL(".gitignore", import.meta.url));
+
 export default defineConfig([
+  includeIgnoreFile(gitignorePath, "Imported .gitignore patterns"),
+  globalIgnores(["*/", "!app/"]),
   {
     extends: compat.extends(
       "next/core-web-vitals",
