669866517b146236c7c3350ec449eae863949213
Adam Basile <basile.adam@gmail.com>
2025-09-04 21:33:53 +1000

    Set up linter

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
-import {Metadata} from "next";
+import { Metadata } from "next";
 
 export const metadata: Metadata = {
-    title: 'Truchet Tiled QR code generator'
+  title: "Truchet Tiled QR code generator",
 };
 
 export default function Home() {
